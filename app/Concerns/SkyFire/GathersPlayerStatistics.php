318c55768dc07ecb13c31ea9fcfb99986e06c94b
Jonas Kervin Hansen <jonas.kerwin.hansen@gmail.com>
2018-06-17 13:53:45 +0200

    Player statistics

---
+++ app/Concerns/SkyFire/GathersPlayerStatistics.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Concerns\SkyFire;
+
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+
+trait GathersPlayerStatistics
+{
+    /**
+     * Get the amount of online players
+     *
+     * @return integer
+     */
+    public function playerOnline()
+    {
+        return DB::connection('skyfire_auth')
+            ->table('account')
+            ->where('online', '>', 0)
+            ->count();
+    }
+
+    /**
+     * Get the amount of active players
+     *
+     * @return int
+     */
+    public function playersActive()
+    {
+        return DB::connection('skyfire_auth')
+            ->table('account')
+            ->whereDate('last_login', '>', Carbon::today()->subMonths(6)->format('Y-m-d'))
+            ->count();
+    }
+
+    /**
+     * Get the amount of inactive players
+     *
+     * @return int
+     */
+    public function playersInactive()
+    {
+        return DB::connection('skyfire_auth')
+            ->table('account')
+            ->whereDate('last_login', '<=', Carbon::today()->subMonths(6)->format('Y-m-d'))
+            ->count();
+    }
+
+    /**
+     * Get the amount of players created within last month
+     *
+     * @return int
+     */
+    public function playersRecentlyCreated()
+    {
+        return DB::connection('skyfire_auth')
+            ->table('account')
+            ->whereDate('joindate', '<=', Carbon::today()->subMonth()->format('Y-m-d'))
+            ->count();
+    }
+
+    /**
+     * Get the total amount of game accounts
+     *
+     * @return int
+     */
+    public function playersTotal()
+    {
+        return DB::connection('skyfire_auth')
+            ->table('account')
+            ->count();
+    }
+}
--- app/Services/SkyFire.php
+++ app/Services/SkyFire.php
@@ -4,8 +4,9 @@ namespace App\Services;
 
 use App\Concerns\SkyFire\SendsIngameMails;
 use App\Concerns\SkyFire\ManagesGameAccounts;
+use App\Concerns\SkyFire\GathersPlayerStatistics;
 
 class SkyFire
 {
-    use ManagesGameAccounts, SendsIngameMails;
+    use ManagesGameAccounts, GathersPlayerStatistics, SendsIngameMails;
 }
