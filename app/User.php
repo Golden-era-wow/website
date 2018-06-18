c6c809204df8cf495d4b38425b242a9e2f4f0cf9
Jonas Kervin Hansen <jonas.kerwin.hansen@gmail.com>
2018-06-18 20:57:46 +0200

    Added balance to users

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'account_name', 'photo_url', 'email', 'password', 'is_admin'
+        'name', 'account_name', 'photo_url', 'email', 'password', 'is_admin', 'balance'
     ];
 
     /**
@@ -47,5 +47,45 @@ class User extends Authenticatable
     public function gameAccounts()
     {
         return $this->hasMany(GameAccount::class, 'user_id');
+    }
+
+    /**
+     * The users current cart
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function cart()
+    {
+        return $this->hasOne(Cart::class)->latest()->available();
+    }
+
+    /**
+     * The users carts
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function carts()
+    {
+        return $this->hasMany(Cart::class)->latest();
+    }
+
+    /**
+     * All the users purchases
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function purchases()
+    {
+        return $this->hasMany(Purchase::class, 'user_id')->latest();
+    }
+
+    /**
+     * All the users payments
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function payments()
+    {
+        return $this->hasMany(Payment::class, 'user_id')->latest();
     }
 }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Jobs\CreateGameAccountJob;
 use Faker\Generator as Faker;
 
 /*
@@ -15,12 +16,23 @@ use Faker\Generator as Faker;
 
 $factory->define(App\User::class, function (Faker $faker) {
         return [
-        'name' => $faker->name,
-        'account_name' => $faker->username,
-        'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'photo_url' => $faker->imageUrl(),
-        'remember_token' => str_random(10),
+            'name' => $faker->name,
+            'account_name' => $faker->username,
+            'balance' => 0,
+            'email' => $faker->unique()->safeEmail,
+            'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+            'photo_url' => $faker->imageUrl(),
+            'remember_token' => str_random(10),
         ];
     }
 );
+
+$factory->state(App\User::class, 'with skyfire account', []);
+$factory->afterCreatingState(App\User::class, 'with skyfire account', function ($user, $faker) {
+    (new CreateGameAccountJob($user, 'secret', ['SkyFire']))->handle();
+});
+
+$factory->state(App\User::class, 'with cart', []);
+$factory->afterCreatingState(App\User::class, 'with cart', function ($user, $faker) {
+    $user->cart()->create(['user_id' => $user->id]);
+});
