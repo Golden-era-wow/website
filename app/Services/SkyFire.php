1433c9e38edc22ae59f06b5c594f2379e177217c
Jonas kervin Hansen <jonas.kerwin.hansen@gmail.com>
2018-06-20 09:39:14 +0200

    find a piece of gear through the emulator

--- app/Services/SkyFire.php
+++ app/Services/SkyFire.php
@@ -2,11 +2,12 @@
 
 namespace App\Services;
 
-use App\Contracts\EmulatorContract;
-use App\Concerns\SkyFire\SendsIngameMails;
-use App\Concerns\SkyFire\ManagesGameAccounts;
 use App\Concerns\SkyFire\GathersPlayerStatistics;
 use App\Concerns\SkyFire\GathersServerStatistics;
+use App\Concerns\SkyFire\ManagesGameAccounts;
+use App\Concerns\SkyFire\SendsIngameMails;
+use App\Contracts\EmulatorContract;
+use Illuminate\Support\Facades\DB;
 
 class SkyFire implements EmulatorContract
 {
@@ -14,4 +15,12 @@ class SkyFire implements EmulatorContract
     const PORT = 8085;
 
     use ManagesGameAccounts, GathersPlayerStatistics, GathersServerStatistics, SendsIngameMails;
+
+    public function findGear($id)
+    {
+    	return DB::connection('skyfire_world')
+    	    ->table('item_template')
+    	    ->where('entry', $id)
+    	    ->first();
+    }
 }
