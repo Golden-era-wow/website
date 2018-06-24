9a9c8d3d49501b8ef3dd63bde06ca995b77ca4ed
Jonas Kervin Hansen <jonas.kerwin.hansen@gmail.com>
2018-06-24 16:01:31 +0200

    Seed the Laravel passport tables

---
+++ database/seeds/PassportTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Laravel\Passport\ClientRepository;
+use Illuminate\Support\Facades\Artisan;
+
+class PassportTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(ClientRepository $clients)
+    {
+        // Artisan::call('passport:install');
+        // Artisan::call('passport:client', ['--personal' => true]);
+        $this->createAuthCodeClient($clients);
+        $this->createPersonalAccessClient($clients);
+    }
+
+    protected function createAuthCodeClient($clients)
+    {
+        $userId = 1;
+        $name = 'default';
+        $redirect = config('app.url').'/auth/callback';
+
+        $clients->create(
+            $userId, $name, $redirect
+        );
+    }
+
+    protected function createPersonalAccessClient($clients)
+    {
+        $name = config('app.name').' Personal Access Client';
+
+        $clients->createPersonalAccessClient(
+            null, $name, config('app.url')
+        );
+    }
+}
