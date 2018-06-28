ecec3f640dc21fc6f0f3e2d1d9df8c1b537abb34
Jonas kervin Hansen <jonas.kerwin.hansen@gmail.com>
2018-06-28 09:51:09 +0200

    Added Passport scope middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -58,6 +58,8 @@ class Kernel extends HttpKernel
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'scope' => \Laravel\Passport\Http\Middleware\CheckForAnyScope::class,
+        'scopes' => \Laravel\Passport\Http\Middleware\CheckScopes::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
     ];
